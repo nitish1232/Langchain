83e42bef9b0731d72e7149cfd081acaeaf3fe0ab
nitish1232 <nitish1232@gmail.com>
2025-08-16 15:50:30 +0530

    Adding prompt template multiple input example

---
+++ 03_prompt_with_multiple_variables.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+
+st.write("Travel guide")
+location = st.text_input("Enter a place to visit")
+month = st.selectbox("Select month of visit", ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+                                               "Sept", "Oct", "Nov", "Dec"])
+budget = st.slider("How much you can spend (in INR)", min_value=100000, max_value=1000000)
+
+if location and month and budget:
+    llm = ChatOpenAI(model="gpt-4o", openai_api_key=os.getenv("OPENAI_KEY"))
+    prompt = PromptTemplate(
+        input_variables=["location", "month", "budget"],
+        template="""
+            You are a travel guide. You have to suggest users with following information when they
+            select a {location}.
+            1. Is it best time to visit during {month} 
+            2. Famous sight seeing locations.
+            3. Famous local foods to try.
+            4. Hotels and restaurants within budget {budget}
+        """
+    )
+    response = llm.invoke(prompt.format(location=location,
+                                        month=month,
+                                        budget=budget))
+    st.write(response.content)
