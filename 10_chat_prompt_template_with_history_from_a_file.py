d15d2c1e7ba757886434cb74b74962468aae8e5e
nitish1232 <nitish1232@gmail.com>
2025-08-17 17:44:09 +0530

    A small chat app with LLM

--- 10_chat_prompt_template_with_history_from_a_file.py
+++ 10_chat_prompt_template_with_history_from_a_file.py
@@ -5,6 +5,9 @@ from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage
+import streamlit as st
+import uuid
 
 load_dotenv()
 
@@ -30,12 +33,31 @@ runnable_chain = RunnableWithMessageHistory(
     history_messages_key="history"
 )
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'quit':
-        break
-    response = runnable_chain.invoke(
-        input={"input": user_input},
-        config={"configurable": {"session_id":"chat123"}}
-    )
-    print(f"Bot: {response}")
+
+st.set_page_config(page_title="LLM Chatbot", layout="centered")
+st.title("Chat with LLM")
+session_id = ""
+
+if "session_id" not in st.session_state:
+    session_id = uuid.uuid4()
+    st.session_state.session_id = session_id
+
+user_input = st.chat_input("Type your message here...")
+session_history = get_session_history(session_id)
+for msg in session_history.messages:
+    role = "human" if isinstance(msg, HumanMessage) else "ai"
+    with st.chat_message(role):
+        st.markdown(msg.content)
+
+if user_input:
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    with st.spinner("Loading..."):
+        response = runnable_chain.invoke(
+            input={"input": user_input},
+            config={"configurable": {"session_id":session_id}}
+        )
+
+    with st.chat_message("ai"):
+        st.markdown(response)
